0e4423c2ecdf8ceaaa12d15b0b61c640e2282d51
David Lluna <llunaplanet@gmail.com>
2019-12-11 09:10:15 +0100

    Arreglar los tests dependientes del docker_host

--- test/spec/04_test11_spec.rb
+++ test/spec/04_test11_spec.rb
@@ -2,15 +2,17 @@ require 'spec_helper'
 
 describe "XI. Treat logs as event streams" do
   
+  let(:docker_host) { ENV.fetch("DOCKER_HOST").gsub("tcp","http") }
+  
   it "Esperamos ver el stack trace del 500 en stdout" do
     response = HTTParty.get('http://sut-a/dameun500')
     expect(response.code).to eq(500)
     
-    container_list_response = HTTParty.get('http://172.17.0.2:2375/v1.38/containers/json')
+    container_list_response = HTTParty.get("#{docker_host}/v1.38/containers/json")
     container_list = JSON.parse(container_list_response.body)
-    sut_container = container_list.select { |item| item["Image"] == "test11_sut-a" || item["Image"] == "testall_sut-a"}.first
+    sut_container = container_list.select { |item| item["Names"].include?("/test11_sut-a_1") || item["Names"].include?("/testall_sut-a_1")}.first
     container_id = sut_container["Id"]
-    container_logs_response = HTTParty.get("http://172.17.0.2:2375/v1.38/containers/#{container_id}/logs?stdout=true")
+    container_logs_response = HTTParty.get("#{docker_host}/v1.38/containers/#{container_id}/logs?stdout=true")
     expect(container_logs_response.code).to eq(200)
     expect(container_logs_response.body).to include("Toma un 500!")
 
