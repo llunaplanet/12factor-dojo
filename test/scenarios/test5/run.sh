6fbbcd6b23b0addb74063235de5d936ae89dda32
David Lluna <llunaplanet@gmail.com>
2019-12-11 09:14:19 +0100

    Añade soporte para diferentes lenguajes
    - Refactor de arnés de test

--- test/scenarios/test5/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export COMPOSE_FILE=./test/scenarios/test5/docker-compose.yml
-docker-compose -p test5 build sut-a sut-b
-docker-compose -p test5 up -d
-docker run -it --rm --network test5_default tester rspec spec/03_test5_spec.rb
-docker-compose -p test5 down
--- test/scenarios/test9/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export COMPOSE_FILE=./test/scenarios/test9/docker-compose.yml
-docker-compose -p test9 build sut-a
-docker-compose -p test9 up -d
-docker run -it --rm --network test9_default tester rspec spec/05_test9_spec.rb
-docker-compose -p test9 down
