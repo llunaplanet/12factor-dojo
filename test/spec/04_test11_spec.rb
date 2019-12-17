d1a014ecc45878a1f7e1bf5bf0e16ae01e40687b
David Lluna <llunaplanet@gmail.com>
2019-12-17 21:45:23 +0100

    Usa el nuevo helper para sacar container_id

--- test/spec/04_test11_spec.rb
+++ test/spec/04_test11_spec.rb
@@ -8,11 +8,10 @@ describe "XI. Treat logs as event streams" do
     response = HTTParty.get('http://sut-a/dameun500')
     expect(response.code).to eq(500)
     
-    container_list_response = HTTParty.get("#{docker_host}/v1.38/containers/json")
-    container_list = JSON.parse(container_list_response.body)
-    sut_container = container_list.select { |item| item["Names"].include?("/test11_sut-a_1") || item["Names"].include?("/testall_sut-a_1")}.first
-    container_id = sut_container["Id"]
+    # Get container logs
+    container_id = get_container_id(docker_host, ["/test11_sut-a_1","/testall_sut-a_1"])
     container_logs_response = HTTParty.get("#{docker_host}/v1.38/containers/#{container_id}/logs?stdout=true")
+    
     expect(container_logs_response.code).to eq(200)
     expect(container_logs_response.body).to include("Toma un 500!")
 
