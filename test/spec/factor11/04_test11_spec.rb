9169bc849a0781564b4a3b2b065e0d8dc0d74a2a
David Lluna <llunaplanet@gmail.com>
2019-12-25 21:02:01 +0100

    Añade timeout para mejorar la experiencia

--- test/spec/factor11/04_test11_spec.rb
+++ test/spec/factor11/04_test11_spec.rb
@@ -5,12 +5,12 @@ describe "XI. Treat logs as event streams" do
   let(:docker_host) { ENV.fetch("DOCKER_HOST").gsub("tcp","http") }
   
   it "Esperamos ver el stack trace del 500 en stdout" do
-    response = HTTParty.get('http://sut-a/dameun500')
+    response = HTTParty.get('http://sut-a/dameun500', timeout: 2)
     expect(response.code).to eq(500)
     
     # Get container logs
     container_id = get_container_id(docker_host, ["/factor11_sut-a_1","/testall_sut-a_1"])
-    container_logs_response = HTTParty.get("#{docker_host}/v1.38/containers/#{container_id}/logs?stdout=true")
+    container_logs_response = HTTParty.get("#{docker_host}/v1.38/containers/#{container_id}/logs?stdout=true", timeout: 2)
     
     expect(container_logs_response.code).to eq(200)
     expect(container_logs_response.body).to include("Toma un 500!")
