d607c51c2b1247fd669a6e9fa9c67628fd7e6674
David Lluna <david.lluna@flywire.com>
2020-09-15 22:59:20 +0200

    Translate spec and endpoint for test 4

--- test/spec/factor4/00_spec.rb
+++ test/spec/factor4/00_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe "IV. Backing services" do
-  
-  it "Probado SUT contra redis version 5.0" do
-    response = HTTParty.get('http://sut-a/masuno', timeout: 2)
+
+  it "SUT works with redis version 5.0" do
+    response = HTTParty.get('http://sut-a/plusone', timeout: 2)
     expect(response.code).to eq(200)
   end
-  
-  it "Probado SUT contra redis version 4.0" do
-    response = HTTParty.get('http://sut-b/masuno', timeout: 2)
+
+  it "SUT works with redis version 4.0" do
+    response = HTTParty.get('http://sut-b/plusone', timeout: 2)
     expect(response.code).to eq(200)
   end
 
