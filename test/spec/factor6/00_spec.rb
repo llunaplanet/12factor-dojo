41aec0f0fa9aca4af25722717c1a1d31982e9bba
David Lluna <david.lluna@flywire.com>
2020-09-15 23:23:43 +0200

    Translate spec and endpoint for test 6

--- test/spec/factor6/00_spec.rb
+++ test/spec/factor6/00_spec.rb
@@ -1,50 +1,16 @@
 require 'spec_helper'
 
 describe "VI. Processes" do
-  # before(:all) do
-  # 
-  #   # wait for suts to be ready
-  #   starting = Time.now
-  #   response = HTTParty.get('http://sut-a/masuno') 
-  #   puts "Waiting for SUT A"
-  #   while response.code != 200
-  #     response = HTTParty.get('http://sut-a/masuno') 
-  #     ending = Time.now
-  #     elapsed = ending - starting
-  #     if(elapsed > 4) 
-  #       break
-  #     end
-  #   end
-  #   puts response.code
-  # 
-  #   puts "Waiting for SUT B"
-  #   starting = Time.now
-  #   response = HTTParty.get('http://sut-b/masuno') 
-  # 
-  #   while response.code != 200
-  #     response = HTTParty.get('http://sut-b/masuno') 
-  #     ending = Time.now
-  #     elapsed = ending - starting
-  #     if(elapsed > 4) 
-  #       break
-  #     end
-  #   end
-  # 
-  #   puts response.code
-  # 
-  #   sleep(10)
-  # 
-  # end
 
   willy_cookies = Hash.new
-  
-  it "Me presento como Willy y el sut me devuelve el saludo" do
-    response_willy = HTTParty.get('http://proxy/diminombre?nombre=Willy', timeout: 2)
+
+  it "I tell the SUT my name and the SUT greets me back" do
+    response_willy = HTTParty.get('http://proxy/saymyname?name=Willy', timeout: 2)
     expect(response_willy.code).to eq(200)
-    expect(response_willy.body).to eq("Hola Willy, encantado de conocerte")
-    
+    expect(response_willy.body).to eq("Hi Willy, nice to meet you!")
+
     set_cookie_header = response_willy.get_fields('Set-Cookie')
-    
+
     set_cookie_header[0].split(',').each {
       |single_cookie_string|
       cookie_part_string  = single_cookie_string.strip.split(';')[0]
@@ -53,16 +19,16 @@ describe "VI. Processes" do
       value               = cookie_part[1]
       willy_cookies[key] = value
     }
-  
+
   end
-  
-  it "El sut me ha enviado una cookie" do  
+
+  it "The SUT sent me a cookie" do
     expect(willy_cookies).to have_key("connect.sid")
   end
-  
-  it "El sut es capaz de recordar mi nombre" do
-    response_a = HTTParty.get('http://proxy/quiensoy', timeout: 2, cookies: willy_cookies)
-    response_b = HTTParty.get('http://proxy/quiensoy', timeout: 2, cookies: willy_cookies)
+
+  it "The SUT is able to remember my name" do
+    response_a = HTTParty.get('http://proxy/whoami', timeout: 2, cookies: willy_cookies)
+    response_b = HTTParty.get('http://proxy/whoami', timeout: 2, cookies: willy_cookies)
     expect(response_a.code).to eq(200)
     expect(response_a.body).to eq("Willy")
     expect(response_b.body).to eq("Willy")
